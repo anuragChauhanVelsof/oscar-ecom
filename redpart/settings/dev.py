90a0c03a0a99d00b9fb4e72c809ea0a8607df51e
Anurag Chauhan <anurag.chauhan@velsof.com>
2025-02-10 11:23:28 +0530

    added csrf trusted origins

--- redpart/settings/dev.py
+++ redpart/settings/dev.py
@@ -1,13 +1,17 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = False
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-(gec!q^xutclbfh+i4fslkht3y+8obwkjv266^oa&=)vv&nqwv"
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://wagtaildemo.veldev.com",
+    "https://www.wagtaildemo.veldev.com"
+]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
